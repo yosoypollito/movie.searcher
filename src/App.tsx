d319572b6db44bdace9f36460850485a885a09ce
Daniel Iglesias <dev.daif@gmail.com>
2023-04-20 04:34:11 -0400

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="flex flex-col">
+      <input
+        className="rounded-lg px-2 py-0.5 w-1/2 border-2 border-slate-800 outline-none"
+        placeholder="Avengers, Horror, Saw ..."
+      />
+    </div>
+  );
+}
+
+export default App;
