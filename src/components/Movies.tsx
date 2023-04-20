f385d55e587e73d4e37eb48a77d07d38b99e2f73
Daniel Iglesias <dev.daif@gmail.com>
2023-04-20 08:02:09 -0400

    fix: adding hasMovies handler, adding full width to movie item

--- src/components/Movies.tsx
+++ src/components/Movies.tsx
@@ -3,16 +3,17 @@ import type { Movies } from "@/src/types";
 import Movie from "./Movie";
 
 export default function Movies({ movies }: { movies: Movies }) {
+  const hasMovies = movies?.length > 0;
   return (
     <>
-      {movies.length === 0 ? (
-        <>No movies found</>
-      ) : (
+      {hasMovies ? (
         <div className="grid grid-cols-[repeat(auto-fill,_minmax(200px,1fr))] w-full gap-4 justify-items-center p-2">
           {movies.map((movie) => {
             return <Movie key={movie.id} {...movie} />;
           })}
         </div>
+      ) : (
+        <>No movies found</>
       )}
     </>
   );
