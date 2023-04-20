b145009ffe7f6d6125e9ecced43a80efdd7dd33d
Daniel Iglesias <dev.daif@gmail.com>
2023-04-20 07:54:47 -0400

    style: changing button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,7 +5,7 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 export default function Button({ children, ...props }: ButtonProps) {
   return (
     <button
-      className="flex border-none px-4 py-1 rounded-xl gap-2 items-center bg-gray-200 w-min"
+      className="flex border-none px-4 py-1 rounded-xl gap-2 items-center bg-gray-200 w-min text-black font-semibold uppercase text-xs"
       {...props}
     >
       {children}
